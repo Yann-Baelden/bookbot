b4c6ceef63c6505de32f66f88a0cf2d0d839203c
Yann-Baelden <yannougdar@yahoo.fr>
2025-01-30 18:25:36 +0100

    add function : count characters in text

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    num_char = get_num_characters(text)
+    print(num_char)
 
+def get_num_characters(text):
+    lower_text = text.lower()
+    characters = []
+    num_characters = {}
+    for character in lower_text:
+        if character in characters:
+            num_characters[character] +=1
+        else:
+            characters.append(character)
+            num_characters[character] = 1
+    return num_characters
 
 def get_num_words(text):
     words = text.split()
