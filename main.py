1f67e17f4558ca73c22a766c80a5bb1f1c425b19
Yann-Baelden <yannougdar@yahoo.fr>
2025-03-16 14:04:25 +0100

    update README

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) > 1:
+        book_path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -12,7 +18,7 @@ def main():
     for item in chars_sorted_list:
         if not item["char"].isalpha():
             continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
+        print(f"{item['char']}: {item['num']}")
 
     print("--- End report ---")
 
